d31478f94a7ef9a0491cff0109ccf44182b1f6c8
JooHub9 <joanasantos.92@hotmail.com>
2022-11-26 22:26:10 +0000

    interfaces created

---
+++ src/app/interfaces.ts
@@ -0,0 +1,19 @@
+interface Catalogo {
+  page: number,
+  total: string,
+  pages: number,
+  limit: string,
+  data: Casa[]
+
+}
+
+interface Casa {
+  id : number,
+  title: string,
+  country: string,
+  city: string,
+  rating: number,
+  price: number,
+  host_type: string,
+  featured_photo: string
+}
