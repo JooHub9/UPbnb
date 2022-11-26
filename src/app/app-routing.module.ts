e8f4348170e9b137086dbd9fcd430367d423de39
JooHub9 <joanasantos.92@hotmail.com>
2022-11-26 22:26:38 +0000

    app component update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CatalogoComponent} from "./catalogo/catalogo.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/catalogo', pathMatch: 'full'},
+  {path: 'catalogo', component: CatalogoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegacaoComponent } from './navegacao/navegacao.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-//Importei isto para os SVG files
-import { HttpClientModule } from '@angular/common/http';
-import { AngularSvgIconModule } from 'angular-svg-icon';
 import { CasaComponent } from './casa/casa.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import {RouterLink} from "@angular/router";
+import {CommonModule} from "@angular/common";
+
 
 
 @NgModule({
@@ -17,14 +19,20 @@ import { CasaComponent } from './casa/casa.component';
     AppComponent,
     NavegacaoComponent,
     CasaComponent,
+    CatalogoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    HttpClientModule, AngularSvgIconModule.forRoot(), //colocado aqui para SVG files
-  ],
-  providers: [],
+    HttpClientModule,
+    RouterLink,
+    CommonModule,
+
+   ],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-pt' }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
