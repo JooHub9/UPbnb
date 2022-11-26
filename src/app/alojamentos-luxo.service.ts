9a84a5a80d8543831c92eb3263eba82990e4cf3f
JooHub9 <joanasantos.92@hotmail.com>
2022-11-26 22:25:46 +0000

    service (http)

--- src/app/alojamentos-luxo.service.ts
+++ src/app/alojamentos-luxo.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+const API_BASE = "https://m9-frontend.upskill.appx.pt/upbnb";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlojamentosLuxoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getCasas() {
+    return this.http.get(API_BASE + "/casas");
+  }
 }
